9ebeb3436a952e1fdb9c1595f2d750a779ee6731
nsedan <naim.alejandro.sedan@gmail.com>
2020-10-25 01:48:22 +0000

    Django testing finished

--- todo/test_models.py
+++ todo/test_models.py
@@ -9,3 +9,7 @@ class TestDjango(TestCase):
     def test_done_default_to_false(self):
         item = Item.objects.create(name='Test Item')
         self.assertFalse(item.done)
+
+    def test_item_string_method_returns_name(self):
+        item = Item.objects.create(name='Test Item')
+        self.assertEqual(str(item), 'Test Item')
